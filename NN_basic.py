e4cc8b915c3f6a5c2f9c9236a53ae017bf6eb82e
szatnikg <gergo.szatnik@gmail.com>
2023-02-17 23:14:12 +0100

    structured and renamed NN-file

--- NN_basic.py
+++ NN_basic.py
@@ -1,7 +1,7 @@
 import array
 
 import pandas
-import tensorflow as tf
+from tensorflow import keras, metrics
 import numpy
 
 import DataProcessing
@@ -106,74 +106,89 @@ class sample_NN():
     def soft_acc(self):
         return tf.keras.backend.mean(tf.keras.backend.equal(tf.keras.backend.round(self.y_test), tf.keras.backend.round(self.preds.squeeze())))
 
-    def showTrainTest(self,with_pred=None):
+    def showTrainTest(self, with_pred=None, column_name="x"):
         plt.figure("Neural Network Performance",figsize=(12, 6))
         if with_pred:
-            plt.scatter(self.x_test["TanOra"], self.preds, c='r', label='Predicted data')
-        plt.scatter(self.x_train["TanOra"], self.y_train, c='b', label='Training data')
+            plt.scatter(self.x_test[column_name], self.preds, c='r', label='Predicted data')
+        plt.scatter(self.x_train[column_name], self.y_train, c='b', label='Training data')
         if len(self.OwnPred_x) == 0:
-            plt.scatter(self.x_test["TanOra"], self.y_test, c='g', label='Testing data')
+            plt.scatter(self.x_test[column_name], self.y_test, c='g', label='Testing data')
         plt.title(f"{self.model_name} "+"planed epoch = "+str(self.epoch)+f" Stopped at: {self.es}" )
+        plt.xlabel(column_name)
+        plt.ylabel("Evaluation feature")
         plt.legend()
         plt.show()
-
         print("preds:",self.preds,"\n","test: ",self.y_test)
-    def build_model(self, nn_type="SimpleNN"):
-        if nn_type=="SimpleNN":
-            # input shape: (train_features.shape[-1],)
-            one_lay = tf.keras.layers.Dense(30, kernel_initializer='normal',
-                                                 activation=tf.keras.activations.relu,
-                                            input_shape=(self.x_train.shape[1],))
 
-
-            self.model = tf.keras.Sequential()
-            #self.model.add(tf.keras.layers.Dropout(0.2, input_shape=(1,)))
-            self.model.add(one_lay)
-            #self.model.add(tf.keras.layers.BatchNormalization())
-            self.model.add(tf.keras.layers.Dense(30, kernel_initializer='normal',
-                                                 activation=tf.keras.activations.relu))
-
-            #self.model.add(tf.keras.layers.BatchNormalization())
-            self.model.add(tf.keras.layers.Dense(1, input_shape=(1,),activation="linear"))
-
-            print(self.model.summary())
-        elif nn_type=="SimpleRNN":
-            self.model = tf.keras.Sequential()
-            self.model.add(tf.keras.layers.SimpleRNN(20, return_sequences=True, input_shape=(1,)))
-            self.model.add.tf.keras.layers.Dense(1)
-
-    def implNN(self, loaded_model=False, epoch=90, batch_size=1, learning_rate=0.2, nn_type="simpleNN", earlystop=0):
-        self.epoch = epoch
+    def build_model(self, nn_type="simple", loaded_model=False):
+        # building a NN from scratch or loading an already existing model
 
         if loaded_model:
             # It can be used to reconstruct the model identically.
-            self.model = tf.keras.models.load_model(f"{self.model_name}.h5")
+            self.model = keras.models.load_model(f"{self.model_name}.h5")
 
             print([layer2.name for layer2 in self.model.layers])
             print("loaded model summary: ",self.model.summary())
             return
+
+        if nn_type=="simple":
+
+            #specifying NN architecture
+            input_lay = keras.Input(shape=(self.x_train.shape[1],))
+            first_lay = keras.layers.Dense(16, activation=keras.activations.relu,
+                kernel_initializer=keras.initializers.random_normal)
+            hidden_lays = keras.layers.Dense(32, activation=keras.activations.relu,
+                kernel_initializer=keras.initializers.random_normal)
+            output_lay = keras.layers.Dense(1, input_shape=(1,), activation="linear")
+
+            # building the NN network
+            self.model = keras.Sequential()
+            #self.model.add(keras.layers.Dropout(0.2, input_shape=(1,)))
+            #self.model.add(keras.layers.BatchNormalization())
+            self.model.add(input_lay)
+            self.model.add(first_lay)
+            self.model.add(hidden_lays)
+            self.model.add(output_lay)
+
+            print(self.model.summary())
+            print("weights: \n",self.model.weights)
+
+        elif nn_type=="SimpleRNN":
+            self.model = keras.Sequential()
+            self.model.add(keras.layers.SimpleRNN(20, return_sequences=True, input_shape=(1,)))
+            self.model.add.keras.layers.Dense(1)
+
+    def network_specs(self,  epoch=90, batch_size=1, learning_rate=0.2, earlystop=0):
+        self.epoch = epoch
+
         # single_feature_normalizer = tf.keras.layers.Normalization(input_shape=[1,], axis=None)
         # single_feature_normalizer.adapt(self.x_test)
-        self.build_model(nn_type=nn_type)
 
         # learning schedule for decaying learning_rate
-        lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+        lr_schedule = keras.optimizers.schedules.ExponentialDecay(
             initial_learning_rate=learning_rate,
             decay_steps=10000,
             decay_rate=0.9)
-        self.model.compile(loss=tf.keras.losses.mse,  # mae stands for mean absolute error
-                      optimizer=tf.keras.optimizers.Adam(learning_rate=lr_schedule)  # stochastic GD
+        self.model.compile(loss=keras.losses.mse,  # mae stands for mean absolute error
+                      optimizer=keras.optimizers.Adam(learning_rate=lr_schedule)  # stochastic GD
                       ,metrics=["mse"]) # metrics=['accuracy']
 
-        #training the model
+        # training the model
         if earlystop:
-            EarlyStop = tf.keras.callbacks.EarlyStopping(patience=30)
+            EarlyStop = keras.callbacks.EarlyStopping(patience=earlystop)
         else: EarlyStop = []
+
+        # NN training start time
+        start = time.time()
         self.history_model = self.model.fit(self.x_train, self.y_train, shuffle=True,
                        epochs=self.epoch,
                        batch_size=batch_size,verbose=0,
                        validation_split=0.2,
                        callbacks=[EarlyStop])
+        ## NN training time stop
+        end = time.time()
+        self.runtime = end - start
+
         if earlystop:
             self.es = EarlyStop.stopped_epoch
             if self.es != 0:
@@ -191,17 +206,17 @@ class sample_NN():
         if len(self.y_test) > 0:
             # get data about predictions
             # self.y_test.reset_index(inplace=True, drop=True)
-            self.mae = tf.metrics.mean_absolute_error(y_true=self.y_test,
+            self.mae = metrics.mean_absolute_error(y_true=self.y_test,
                                                  y_pred=self.preds).numpy()
             # metrics return a tf.Tensor object, so I convert to numpy-ndarray.
-            mse = tf.metrics.mean_squared_error(y_true=self.y_test,
+            mse = metrics.mean_squared_error(y_true=self.y_test,
                                                 y_pred=self.preds).numpy()
             # ToDo implement Classification accuracy
             if "classificationProblem":
-                acc = tf.metrics.Accuracy()
+                acc = metrics.Accuracy()
                 acc.update_state([[3.1,45]], [[3.0,45 ]])
                 print("accuracy: ",acc.result().numpy())
-            # acc = tf.metrics.RootMeanSquaredError()
+            # acc = metrics.RootMeanSquaredError()
             # acc.update_state(self.y_test, self.preds.squeeze())
             # ToDo Denormalize back the metrics with y_train features
             #  ONLY if OwnPrediction was given
@@ -221,28 +236,29 @@ if __name__ == "__main__":
     pred_x = [] # own_pred_data.iloc[::, :-2]
     pred_y = [] #own_pred_data.iloc[::,-2]  #pandas.DataFrame({"y": [j**2 for j in range(460, 560)]})
 
-    # NN training start time
-    start = time.time()
+
 
     NN = sample_NN("TryoutBasic")
     # Preprocess
     x = data[data.columns[:-2]]
     y = data[data.columns[-2]]
     NN.preprocessing(x,
-                     y, normalize=True, features=[], OwnPred_x=pred_x, OwnPred_y=pred_y, label_feature_name="Teljesitmeny")
+                     y, normalize=True, features=[],
+                     OwnPred_x=pred_x, OwnPred_y=pred_y,
+                     label_feature_name="Teljesitmeny")
     NN.split_train_test(train_split=0.7, shuffle=True)
-    NN.implNN(loaded_model=False, epoch=50, batch_size=5, learning_rate=0.0035, nn_type="SimpleNN", earlystop=1)
+
+    NN.build_model()
+    NN.network_specs(epoch=50, batch_size=5, learning_rate=0.0035, earlystop=20)
+    print("Training Time:",
+          str(round(NN.runtime, 4)), "seconds."
+          )
     NN.predictNN()
 
-    end = time.time()
-    runtime = end-start
-    print("Training Time:",
-          str(round(runtime, 4)), "seconds."
-          )
     NN.showValLoss()
     NN.postprocessing(is_preds_normalized=True)
     NN.evaluate()
-    NN.showTrainTest(with_pred=True)
+    NN.showTrainTest(with_pred=True, column_name=data.columns[0])
 
 
     # shuffle order is right for the showTrainTest method, but using pandas.concat,
